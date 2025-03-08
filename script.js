d84877d04a9f1f8da4ec0d840ab0fe0394d980f1
Saurabh Kale <saurabhkale403@gmail.com>
2025-03-09 02:30:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById('background');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particlesArray.forEach(p => p.update());
+    particlesArray.forEach(p => p.draw());
+    requestAnimationFrame(animate);
+}
+
+for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+}
+
+animate();
+
+
